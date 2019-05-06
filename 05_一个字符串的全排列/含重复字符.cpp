a62839eab9367933bacb2708b51529e83aa5486b
BornToDeath <38156251+BornToDeath@users.noreply.github.com>
2019-05-06 21:42:42 +0800

    Create 含重复字符.cpp

---
+++ 05_一个字符串的全排列/含重复字符.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <algorithm>
+using std::vector;
+using std::string;
+
+/*
+ * 输入一个字符串,按字典序打印出该字符串中字符的所有排列。
+ * 例如输入字符串abc,则打印出由字符a,b,c所能排列出来的所
+ * 有字符串abc,acb,bac,bca,cab和cba。
+ *
+ * 输入描述：
+ * 输入一个字符串,长度不超过9(可能有字符重复),字符只包括大小写字母。
+ */
+
+class Solution{
+    public:
+        vector<string> Permutation(string str){
+            vector<string> res;
+            if(str.empty()){
+                return res;
+            }
+            QuanPaiLie(str, 0, res);
+            return res;
+        }
+    private:
+        void QuanPaiLie(string str, int step, vector<string> &v){
+            if(step == str.length()){
+                //下面三行代码是与“不含重复字符.cpp”不同之处
+                if(find(v.begin(), v.end(), str) == v.end()){
+                    v.push_back(str);
+                }
+                return;
+            }
+            for(int i=step;i<str.length();i++){
+                char c = str[i];
+                str[i] = str[step];
+                str[step] = c;
+                QuanPaiLie(str, step+1, v);
+            }
+        }
+};
+
+int main(){
+
+    std::cout<<"Input a string with length no more than 9:";
+    string str;
+    std::cin>>str;
+
+    Solution s;
+    vector<string> v = s.Permutation(str);
+    for(int i=0;i<v.size();i++){
+        std::cout<<i+1<<":"<<v[i]<<std::endl;
+    }
+
+    return 0;
+}
